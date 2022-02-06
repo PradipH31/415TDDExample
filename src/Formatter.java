0fa452b019a2b46b585cd4e8d4a56b67a56cbc5e
pradiph31 <pradip.hamal31@gmail.com>
2022-02-06 14:04:41 -0600

    Burris version

--- src/Formatter.java
+++ src/Formatter.java
@@ -1,21 +1,23 @@
+import java.util.Arrays;
 
 public class Formatter {
+
 	int lineWidth;
+	private char spaces[];
 
 	public void setLineWidth(int lineWidth) {
 		this.lineWidth = lineWidth;
+		spaces = new char[lineWidth];
+		Arrays.fill(spaces, ' ');
 	}
 
 	public String center(String string) {
-		int length = string.length();
-		if (length < lineWidth) {
-			length = (lineWidth - length) / 2;
-			String whiteSpace = "";
-			for (int i = 0; i < length; i++) {
-				whiteSpace = whiteSpace + " ";
-			}
-			return whiteSpace + string + whiteSpace;
-		}
-		return string;
+		StringBuffer b = new StringBuffer();
+		int padding = (lineWidth - string.length()) / 2;
+		b.append(spaces, 0, padding);
+		b.append(string);
+		b.append(spaces, 0, padding);
+		return b.toString();
 	}
+
 }
