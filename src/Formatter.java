e90831534a0ce2a427183c15bce52fce51bbd3cb
pradiph31 <pradip.hamal31@gmail.com>
2022-02-06 14:04:19 -0600

    My version for the solution

--- src/Formatter.java
+++ src/Formatter.java
@@ -1,4 +1,21 @@
 
 public class Formatter {
+	int lineWidth;
 
+	public void setLineWidth(int lineWidth) {
+		this.lineWidth = lineWidth;
+	}
+
+	public String center(String string) {
+		int length = string.length();
+		if (length < lineWidth) {
+			length = (lineWidth - length) / 2;
+			String whiteSpace = "";
+			for (int i = 0; i < length; i++) {
+				whiteSpace = whiteSpace + " ";
+			}
+			return whiteSpace + string + whiteSpace;
+		}
+		return string;
+	}
 }
